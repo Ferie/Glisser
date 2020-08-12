f2844ca4627e7adee5944f57bab7a08e3d95b08d
Riccardo Andreatta <ferie150@hotmail.com>
2020-08-12 15:43:26 +0100

    Added route module and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: UserDetailsComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
