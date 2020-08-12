7e57365981f9044927d61285df4678a193c474ca
Riccardo Andreatta <ferie150@hotmail.com>
2020-08-12 15:44:29 +0100

    Removed standard app.component and styled for the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'glisser-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'glisser-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'glisser';
+    title = 'glisser';
 }
